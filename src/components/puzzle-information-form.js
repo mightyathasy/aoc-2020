d9f3a6482d51d1b9edacc3d777c8dc97338265c6
mightyathasy <akos.toth953@gmail.com>
2020-12-06 17:08:51 +0100

    [UI] Fields positioned to the center

--- src/components/puzzle-information-form.js
+++ src/components/puzzle-information-form.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import '../styles/index.css'
+import '../styles/component-style.css'
+
 class PuzzleInformationForm extends React.Component {
     constructor(props) {
         super(props);
@@ -20,13 +23,28 @@ class PuzzleInformationForm extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Number of Puzzle:
-                   <input type="text" value={this.state.value} onChange={this.handleChange} size={40} placeholder={"type something like 1/a or 3/B"}/>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <div className="outer-div">
+                <div className="form-column">
+                    <form onSubmit={this.handleSubmit}>
+                        <label >
+                            Number of Puzzle:
+                            <input type="text" value={this.state.value} onChange={this.handleChange} size={40} placeholder={"type something like 1/a or 3/B"}/>
+                        </label>
+                        <input type="submit" value="Submit" />
+                        <br/>
+                        <label>
+                            Input of Puzzle:
+                            <br/>
+                            <textarea placeholder={"Just paste the input here."}/>
+                        </label>
+                    </form>
+                </div>
+                <div className="result-column">
+                    <label>
+                        Placeholder of result output
+                    </label>
+                </div>
+            </div>
         );
     }
 }
